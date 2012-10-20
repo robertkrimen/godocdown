06b613807081a59aa4a99e469a25d9d07f443689
Robert Krimen <robertkrimen@gmail.com>
2012-10-20 10:42:26 -0700

    Add template documentation

--- godocdown/documentation.go
+++ godocdown/documentation.go
@@ -28,13 +28,48 @@ Usage
 The following options are accepted:
 
 	-heading="TitleCase1Word"
-	Heading detection method: 1Word, TitleCase, Title, TitleCase1Word, ""
+	// Heading detection method: 1Word, TitleCase, Title, TitleCase1Word, ""
 
 	-no-template=false
-	Disable template processing
+	// Disable template processing
 
 	-plain=false
-	Emit standard Markdown, rather than Github Flavored Markdown (the default)
+	// Emit standard Markdown, rather than Github Flavored Markdown (the default)
+
+Templating
+
+In addition to Markdown rendering, godocdown provides templating via text/template (http://golang.org/pkg/text/template/)
+for further customization. By putting a file named ".godocdown.template" in the same directory as your
+package/command, godocdown will know to use the file as a template. The following names are also accepted,
+with the first one encountered being used:
+
+	# text/template
+	.godocdown.markdown
+	.godocdown.md
+	.godocdown.template
+	.godocdown.tmpl
+
+In addition to the standard template functionality, the starting data argument has the following interface:
+
+	.Emit
+	// A method emitting all of the rendered documentation
+
+	.EmitHeader
+	// A method emitting the package/command name and an import line (if one is present/needed)
+
+	.EmitSynopsis
+	// A method emitting the package/command declaration
+
+	.EmitUsage
+	// A method emitting package usage, include a constant section, a variable section,
+	// the function section, and a type section (and each type having its own constant, variable,
+	// and function/method listing)
+
+	.IsCommand
+	// A boolean indicating whether the given package is a command or a plain package
+
+	.Name
+	// A string containing the name of the package/command
 
 */
 package documentation
